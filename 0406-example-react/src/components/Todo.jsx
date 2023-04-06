b26f1ffcc7de90ac94e996aa2fa490703e193874
KIMPPT <dudghks9303@gmail.com>
2023-04-06 17:00:28 +0900

    04061700

--- 0406-example-react/src/components/Todo.jsx
+++ 0406-example-react/src/components/Todo.jsx
@@ -3,13 +3,13 @@ import "./todo.css";
 
 let date = new Date();
 let id = 3;
-let list2=[
-    { id: 1, date: "4월 5일", todo: "첫번째 할 일", checked: false },
-    { id: 2, date: "4월 6일", todo: "두번째 할 일", checked: true },
-  ]
-let newlist2=list2;
-let newlist3=list2;
-let newlist4=list2;
+let list2 = [
+  { id: 1, date: "4월 5일", todo: "첫번째 할 일", checked: false },
+  { id: 2, date: "4월 6일", todo: "두번째 할 일", checked: true },
+];
+let newlist2 = list2;
+let newlist3 = list2;
+let newlist4 = list2;
 let todaylist = () => {
   let month = date.getMonth();
   let day = date.getDate();
@@ -18,15 +18,15 @@ let todaylist = () => {
 let Todo = () => {
   let [todolists, setTodolist] = useState(list2);
   let [inputlist, setInputlist] = useState("");
-  console.log("todolists",todolists); // State로 받은 todolists 값
-  console.log("list2",list2); //변하지 않는 초기 리스크 값
-  console.log("newlist3",newlist3); //check변경 이벤트 로 생성된 list 값
-  console.log("newlist2",newlist2); //추가이벤트로 생성된 list값
-  console.log("newlist4",newlist4); //오늘 할 일 버튼 이벤트로 생성된 list 값
+  console.log("todolists", todolists); // State로 받은 todolists 값
+  console.log("list2", list2); //변하지 않는 초기 리스크 값
+  console.log("newlist3", newlist3); //check변경 이벤트 로 생성된 list 값
+  console.log("newlist2", newlist2); //추가이벤트로 생성된 list값
+  console.log("newlist4", newlist4); //오늘 할 일 버튼 이벤트로 생성된 list 값
   return (
     <div className="todolist">
       <h1>Todo-List</h1>
-      
+
       <input
         type="text"
         onChange={(e) => {
@@ -56,10 +56,12 @@ let Todo = () => {
       <hr />
       <button
         onClick={() => {
-            (todolists==list2)?setTodolist(list2):(
-                ((todolists== newlist2)?setTodolist(newlist2):setTodolist(newlist3))
-            )
-            //조금 까다로운 조건부[할 일 추가하냐 안하냐 + 할일 추가 전에 체크의 유무 확인]
+          todolists == list2
+            ? setTodolist(list2)
+            : todolists == newlist2
+            ? setTodolist(newlist2)
+            : setTodolist(newlist3);
+          //조금 까다로운 조건부[할 일 추가하냐 안하냐 + 할일 추가 전에 체크의 유무 확인]
         }}
       >
         모든 할 일
@@ -68,7 +70,7 @@ let Todo = () => {
         onClick={() => {
           let newlist = todolists.filter((list) => list.date === todaylist());
           setTodolist(newlist);
-          newlist4=newlist;
+          newlist4 = newlist;
         }}
       >
         오늘 할 일
@@ -92,9 +94,8 @@ let Todo = () => {
               });
               //위에서 바꾸어서 만든 새 배열을 기존 배열에 덮음
               setTodolist(newlist);
-              
-              newlist2=newlist;
-              
+
+              newlist2 = newlist;
             }}
           />
           {todolist.todo}
@@ -102,7 +103,6 @@ let Todo = () => {
             onClick={() => {
               let newList = todolists.filter((list) => list.id !== todolist.id);
               setTodolist(newList);
-              
             }}
           >
             X
